55e121aaaeb5aae0afdc3a06ed99e4a431dc3a45
Julian Huiskamp <julian.huiskamp@kadaster.nl>
2023-11-13 18:21:05 +0100

    RestTemplate configuration setup.

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -23,16 +23,12 @@ public class RestTemplateBuilderConfig {
     String password;
 
     @Bean
-    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
 
         assert rootUrl != null;
 
-        RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
-        DefaultUriBuilderFactory uriBuilderFactory = new
-                DefaultUriBuilderFactory(rootUrl);
-
-        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username,password);
-
-        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
+        return configurer.configure(new RestTemplateBuilder()
+                .basicAuthentication(username, password)
+                .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl)));
     }
 }
